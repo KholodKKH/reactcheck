e3cf6baabe0601fd932cab8f31e61bae3ef8d606
KholodKKH <107035687+KholodKKH@users.noreply.github.com>
2022-06-27 16:05:28 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import a from "./imeges/a.jpg";
+import b from "./imeges/b.jpg";
+import c from "./imeges/c.jpg";
+import {Nav,Navbar,Container,Card,CardGroup,Form,Row,Col,Button} from 'react-bootstrap';
+import'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* --- Navbar Part ----- */} 
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home" className='clinic'>AL-Sendan Clinic</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className='homepage'>Home page</Nav.Link>
+            <Nav.Link href="#home" className='about'>about as</Nav.Link>
+            <Nav.Link href="#features" className='medical'>Medical services</Nav.Link>
+            <Nav.Link href="#pricing" className='doctors'>Our Doctors</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+       {/* --- Cards Part ----- */}
+<div className='cards'>
+    <CardGroup>
+      <Card className='ali' >
+        <Card.Img variant="top" src={a} />
+        <Card.Body>
+          <Card.Title>Dr.Ali Hatem</Card.Title>
+          <Card.Text>
+          General Surgeon Almarsa-Tunisia
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      
+        <Card className='mohanned'>
+        <Card.Img variant="top" src={c}/>
+        <Card.Body>
+          <Card.Title>Dr.Mohanned Abudaber</Card.Title>
+          <Card.Text>
+          Obstetrician- Gynecologist Tripoli-Libya
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className='najla'>
+        <Card.Img variant="top" src={b}/>
+        <Card.Body>
+          <Card.Title >Dr.Najla Ahmed</Card.Title>
+          <Card.Text>
+          Orthopedic Surgeon Cairo-Egypt 
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    
+    </CardGroup>
     </div>
-  );
-}
+    <Card> 
+        <Card.Footer>
+            {/* --- Form Part ----- */}
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="3">
+        <h5>Sign Up For Our Newsletter</h5>
+        </Form.Label>
+        <Col sm="6">
+          <Form.Control type="email" placeholder="Email address" />
+        </Col>
+        <Col sm="3">
+        <Button type="button" class="btn btn-outline-light mb-4">
+              Subscribe
+            </Button>
+        </Col>
+      </Form.Group>
+      {/* --- Footer Part ----- */}
+      <footer className='footer'>
+      <h3>Contact Us :- sendanclinic@gmail.com</h3>
+      <p>Our quality policies lie in providing our health services with apatient-oriented approach in accordance with international standards Open to development and technology We care about patient satisfaction and staff We are mobilized to provide reliable diagnosis, treatment and exemplary care environment
+        Our services provide people with reliable service and respect for patient rights and privacy
+        </p></footer>
+          </Card.Footer>
+          </Card>
+    </div>)}
+
 
 export default App;
